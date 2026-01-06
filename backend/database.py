8339ba394445846f3fe4cf9017c71d85240f4915
Bartek Pieróg <pierogbartoszzstti@gmail.com>
2026-01-06 15:41:39 +0100

    reset app backend

--- backend/database.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import create_engine, SQLModel, Session
-
-sqlite_url = "sqlite:///./database.db" 
-engine = create_engine(sqlite_url, connect_args={"check_same_thread": False})
-def create_db_and_tables(): 
-    SQLModel.metadata.create_all(engine)
-def get_session(): 
-    with Session(engine) as session:
-         yield session
