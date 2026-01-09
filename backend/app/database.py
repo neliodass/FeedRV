cf93283eae7e4acd447ed8dd0ffe94b6a73be77a
Bartek Pieróg <pierogbartoszzstti@gmail.com>
2026-01-09 15:33:49 +0100

    add index for item embeddings and update bcrypt dependency version

--- backend/app/database.py
+++ backend/app/database.py
@@ -8,6 +8,15 @@ def init_db():
         session.exec(text("CREATE EXTENSION IF NOT EXISTS vector;"))
         session.commit()
     SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        session.exec(text("""
+                            CREATE INDEX IF NOT EXISTS item_embedding_idx 
+                            ON item USING hnsw (embedding vector_cosine_ops);
+                        """))
+
+        session.commit()
+
+
 def get_session():
     with Session(engine) as session:
         yield session
